6a2c11f61e82a2cfa6a76831bff385375e5b2041
mktgts_home_desktop <maksimkor96@yandex.ru>
2026-03-12 12:21:11 +0300

    fix chat scrolling 3

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <AuthProvider>
           <div className="flex min-h-screen flex-col">
             <Header />
-            <main className="flex-1 flex flex-col">{children}</main>
+            <main className="flex-1">{children}</main>
             <Footer />
           </div>
           <Toaster />
